8fbebb669450a3ef2d3fcf85f7aff1101bcb1849
Foxies-CSTL <Foxies-CSTL@users.noreply.github.com>
2021-10-22 02:00:06 +0200

    Fix compilation HAL STM32F1

--- Marlin/src/pins/stm32f1/pins_FLSUN_HISPEED.h
+++ Marlin/src/pins/stm32f1/pins_FLSUN_HISPEED.h
@@ -29,7 +29,7 @@
  * #define SERIAL_PORT_2 3
  */
 
-#if NOT_TARGET(__STM32F1__, STM32F1xx)
+#if NOT_TARGET(__STM32F1__, STM32F1)
   #error "Oops! Select an STM32F1 board in 'Tools > Board.'"
 #elif HOTENDS > 1 || E_STEPPERS > 1
   #error "FLSUN HiSpeedV1 only supports one hotend / E-stepper. Comment out this line to continue."
