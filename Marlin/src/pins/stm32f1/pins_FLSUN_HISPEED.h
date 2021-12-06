3e90c8cd71b0ee808e300980e50448ca3338a3eb
Foxies-CSTL <Foxies-CSTL@users.noreply.github.com>
2021-12-06 19:14:46 +0100

    Update pins_FLSUN_HISPEED.h

--- Marlin/src/pins/stm32f1/pins_FLSUN_HISPEED.h
+++ Marlin/src/pins/stm32f1/pins_FLSUN_HISPEED.h
@@ -371,4 +371,21 @@
     #define TFT_BTOKMENU_COLOR            0x145F  // Cyan
   #endif
   #define TFT_BUFFER_SIZE                  14400
+  
 #endif
+/*
+#if HAS_WIRED_LCD
+  #define LCD_PINS_ENABLE        PD13
+  #define LCD_PINS_D4              -1 //PE14 //dataline EXP1_06_PIN  // PA6(LCD_D4)
+  #if IS_ULTIPANEL
+    #define LCD_PINS_D5            -1 //PE15 //dataline EXP1_05_PIN  // PA7(LCD_D5)
+    #define LCD_PINS_D6            -1 //PD11 //rs EXP1_04_PIN  // PC4(LCD_D6)
+    #define LCD_PINS_D7            -1 //PD10 //dataline EXP1_03_PIN  // PC5(LCD_D7)
+
+    #if !defined(BTN_ENC_EN) && ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
+      #define BTN_ENC_EN           LCD_PINS_D7  // Detect the presence of the encoder
+    #endif
+  #endif
+#endif
+*/
+
